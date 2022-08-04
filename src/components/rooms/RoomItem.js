895786f8075485e9db63918d47516aa142e87898
Atulkar Kumar Singh <atlkrrjpt428@gmail.com>
2022-08-04 16:46:12 +0530

    created rooms context

--- src/components/rooms/RoomItem.js
+++ src/components/rooms/RoomItem.js
@@ -1,36 +1,16 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
-import ProfileAvatar from '../ProfileAvatar';
 
-const RoomItem = ({ room }) => {
-  const { createdAt, name, lastMessage } = room;
-
+const RoomItem = () => {
   return (
     <div>
       <div className="d-flex justify-content-between align-items-center">
-        <h3 className="text-disappear">{name}</h3>
+        <h3 className="text-disappear">Room name</h3>
         <TimeAgo datetime={new Date()} className="font-normal text-black-45" />
       </div>
 
       <div className="d-flex align-items-center text-black-70">
-        {lastMessage ? (
-          <>
-            <div className="d-flex align-items-center">
-              <ProfileAvatar
-                src={lastMessage.author.avatar}
-                name={lastMessage.author.name}
-                size="sm"
-              />
-            </div>
-
-            <div className="text-disappear ml-2">
-              <div className="italic">{lastMessage.author.name}</div>
-              <span>{lastMessage.text || lastMessage.file.name}</span>
-            </div>
-          </>
-        ) : (
-          <span>No messages yet...</span>
-        )}
+        <span>No messages yet...</span>
       </div>
     </div>
   );
